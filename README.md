0f9adab6363d541271b6a151a6fecf6d4e777b7e
Lenhan231 <nhanle221199@gmail.com>
2023-12-20 21:11:38 +0700

    changing the destination

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Sorting Algorith
 ## Basic
 -[bubble sorting](bubblesort.py)
+-[quick sort](quicksort.py)
+-
---
+++ filesource/selectionsort.py
@@ -0,0 +1,12 @@
+with open("D:\\sorting algo\\sorting-algo\\dayso.txt", "r") as ds:
+    x = list(map(int, ds.readline().split()))
+    k = []
+def selection(x,k):
+    for i in range(0,len(x)):
+        r = min(x)
+        k.append(r)
+        x.remove(min(x))
+    return k
+selection(x,k)
+print(k)
+
