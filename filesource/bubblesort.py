253ca5c35f456a7bd18e653614fc2d380f70f833
Lenhan231 <nhanle221199@gmail.com>
2024-01-15 21:13:32 +0700

    add sourcenua

--- filesource/bubblesort.py
+++ filesource/bubblesort.py
@@ -1,7 +1,6 @@
 with open("dayso.txt", "r") as ds:
     x = list(map(int, ds.readline().split()))
-
-def swap(a, b):
+'''def swap(a, b):
     return b, a
 
 def bubble(x, k):
@@ -14,4 +13,15 @@ def bubble(x, k):
         return bubble(x, k - 1)
 
 sorted_list = bubble(x, len(x))
-print(sorted_list)
+print(sorted_list)'''
+def bubblesort(x,count):
+    for i in range(0,len(x)-1):
+        if x[i]>x[i+1]:
+            x[i],x[i+1] = x[i+1],x[i]
+            count += 1
+    if count != 0 :
+        bubblesort(x,0)
+        
+
+bubblesort(x,0)
+print(x)
--- filesource/mergesort.py
+++ filesource/mergesort.py
@@ -1,7 +1,6 @@
 with open("dayso.txt", "r") as ds:
     x = list(map(int, ds.readline().split()))
     xcopy = x.copy()
-k= len(x)
 for i in x:
     print(i,end=" ")
-print(k)
+    
