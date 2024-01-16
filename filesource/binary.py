6049d2b40e342be668b3ebf31c3b61db2769210f
Lenhan231 <nhanle221199@gmail.com>
2024-01-16 11:36:58 +0700

    ok

---
+++ filesource/binary.py
@@ -0,0 +1,19 @@
+arr = [2, 3, 4, 8, 9, 10, 13, 40, 100]
+a = 13
+
+def binary(arr, inp):
+    mid = len(arr) // 2
+
+    if len(arr) > 0:
+        if arr[mid] < inp:
+            newarr = arr[mid+1:]
+            binary(newarr, inp)
+        elif arr[mid] > inp:
+            newarr = arr[:mid]
+            binary(newarr, inp)
+        else:
+            print(1)
+    else:
+        print(0)
+
+binary(arr, a)
