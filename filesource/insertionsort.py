194603f17611c80732d9179fdfb22f4ae177bf24
Lenhan231 <nhanle221199@gmail.com>
2024-01-16 23:38:03 +0700

    o ke am fine ne

---
+++ filesource/insertionsort.py
@@ -0,0 +1,14 @@
+arr = [5 ,13 ,29 ,101 , 42,73,99,1012]
+def insertion(arr):
+    for i in range(0,len(arr)-1):
+        if arr[i]>arr[i+1]:
+            arr[i],arr[i+1] = arr[i+1],arr[i]
+            while i != 0:
+                if arr[i]<arr[i-1]:
+                    arr[i],arr[i+1] = arr[i+1],arr[i]
+                    i-=1
+                else:
+                    i-=1
+insertion(arr)
+print(arr)
+
